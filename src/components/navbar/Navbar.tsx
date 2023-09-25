a7516ccb43d627119af327beabc8f0f1e8afd02c
Rich dad <utkirsoft@gmail.com>
2023-09-25 23:12:17 +0500

    navbar style

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
                     <span>1</span>
                 </div>
                 <div className="user">
-                    <img src="/user.svg" alt=""/>
+                    <img src="https://scontent.ftas9-1.fna.fbcdn.net/v/t39.30808-6/352309653_277145754685317_4478000672231544502_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=u6y-3vhC1koAX_1t7VG&_nc_ht=scontent.ftas9-1.fna&oh=00_AfB4bN_UZllV8I4FhV03go-9eE4XYwwqW8Bn2_qM1TeFLA&oe=6515EF10" alt=""/>
                     <span>Sattarov Utkir</span>
                 </div>
                 <img src="/settings.svg" alt="" className="icon"/>
